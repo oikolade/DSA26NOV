48c3716f0cc2f64d3ecb066187bfade76bcee6d4
Tosin <ibitola25@gmail.com>
2022-12-22 04:08:29 +0000

    added

---
+++ devops-training-project-master/Deliverables/vpc_ec2_terraform/dynamic_inventory.sh
@@ -0,0 +1,17 @@
+PUB_IP=`aws ec2 describe-instances --filters Name=tag:Name,Values=tf-instance --region ap-southeast-1 | grep "PublicIpAddress" | awk '{print $2}' | sed 's/"//g' | sed 's/,//g'`
+echo $PUB_IP
+ANS=/var/lib/jenkins/workspace/terraform-ansible-prov-conf/devops-training-project-master/Deliverables/ansible-plays/
+rm -f $ANS/inventory.ini
+echo 'creating inventory.ini...' > $ANS/inventory.ini
+cat <<EOF > $ANS/inventory.ini
+[all:vars]
+ansible_ssh_user=ubuntu
+ansible_ssh_private_key_file=~/.ssh/spring-petclinic.pem
+ansible_connection=ssh
+ansible_user=ubuntu
+ansible_sudo=true
+ansible_become=true
+
+[docker]
+$PUB_IP
+EOF
